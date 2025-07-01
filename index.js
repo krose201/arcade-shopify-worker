15597844a1e185a9dc6d58e5f264b75aa467f209
krose201 <krose.correia1@gmail.com>
2025-07-01 14:38:52 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-iimport { Arcade } from "@arcade/sdk";
-import { getShopifyOrders } from "./getShopifyOrders.js";
-
-const arcade = new Arcade();
-
-arcade.register([getShopifyOrders]);
-
-arcade.listen(); // this tells Arcade to expose this worker's tools
